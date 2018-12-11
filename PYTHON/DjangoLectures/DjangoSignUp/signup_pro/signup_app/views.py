283fb1c98c970cc3a75a88b0723b20cb68e2f843
Arunkumar Arjunan <aruntracer@gmail.com>
2018-12-11 17:43:18 +0530

    Updated

--- PYTHON/DjangoLectures/DjangoSignUp/signup_pro/signup_app/views.py
+++ PYTHON/DjangoLectures/DjangoSignUp/signup_pro/signup_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from signup_app.forms import NewUser
 
-# Create your views here.
+def index(request):
+    return render(request,'signup_app/index.html')
+def users(request):
+    form = NewUser()
+    if request.method == 'POST':
+        form = NewUser(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('Error form invalid')
+    return render(request,'signup_app/users.html',{'form':form})
--- PYTHON/DjangoLectures/DjangoSignUp/signup_pro/signup_pro/urls.py
+++ PYTHON/DjangoLectures/DjangoSignUp/signup_pro/signup_pro/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from signup_app import views
 urlpatterns = [
+    path('', views.index,name = "index"),
     path('admin/', admin.site.urls),
+    path('users.html/',views.users,name="users"),
 ]
