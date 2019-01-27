805cebf0d8863d584df79700ab11d08610c878e4
Arunkumar Arjunan <aruntracer@gmail.com>
2019-01-27 21:18:28 +0530

    Added db api and python to db load code

---
+++ PYTHON/Dbload/Dbloadapp/properties.py
@@ -0,0 +1,8 @@
+import time,os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))#log_pro path
+airport_logs_dir = os.path.join(BASE_DIR,"Dbload","static","AirportLogs")
+report_dir = os.path.join(BASE_DIR,"static","AirportLogs","Report")
+airport_file_name = os.path.join(report_dir,'Airport_Log_'+str.replace(str.replace(time.ctime(),' ','_'),':','_')+'.csv')
+airport_col_name = [['FILE_NAME','DATE','SIM_NUMBER','TN','NAME','PLAN_NAME','SITE_ID','SITE_NAME']]
+print(airport_logs_dir)
