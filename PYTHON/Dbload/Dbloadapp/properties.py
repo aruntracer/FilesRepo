2b6bfd86c18490ad2f92f1ae3bc8d1a70e4843e6
Arunkumar Arjunan <aruntracer@gmail.com>
2019-02-08 12:51:48 +0530

    removed unwanted stuffs

--- PYTHON/Dbload/Dbloadapp/properties.py
+++
@@ -1,7 +0,0 @@
-import time,os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))#log_pro path
-airport_logs_dir = os.path.join(BASE_DIR,"Dbload","static","AirportLogs")
-report_dir = os.path.join(BASE_DIR,"Dbload","static","AirportLogs","Report")
-airport_file_name = os.path.join(report_dir,'Airport_Log_'+str.replace(str.replace(time.ctime(),' ','_'),':','_')+'.csv')
-airport_col_name = [['FILE_NAME','DATE','SIM_NUMBER','TN','NAME','PLAN_NAME','SITE_ID','SITE_NAME']]
--- PYTHON/Dbload/static/AirportLogs/test.sql
+++
@@ -1,5 +0,0 @@
-name: arunkumar sno: 111
-name: bala	    sno: 222
-name: dhilip    sno: 333
-name: ram       sno: 444
-name: vinoth    sno: 555
